f9481145ac9ad2028454a4359efd4a3b653f2771
Chris Villa <chrisvilla@me.com>
2019-12-02 19:27:34 -0800

    fix: ensure API docs are correctly processed

--- scrape.ts
+++ scrape.ts
@@ -6,7 +6,10 @@ import scrapeIt, { ScrapeOptions } from 'scrape-it'
  * @param url - URL to scrape
  * @param opts - Scraping options as described in the [scrape-it readme](https://github.com/IonicaBizau/scrape-it#documentation)
  */
-export default async (url: string, opts: object): Promise<object> => {
+export default async function scrape(
+  url: string,
+  opts: object
+): Promise<object> {
   const { data } = await scrapeIt(url, opts as ScrapeOptions)
 
   return data

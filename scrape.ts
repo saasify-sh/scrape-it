62e6248c868d6a0fecb5672e7d6c8bc12ac4010d
Chris Villa <chrisvilla@me.com>
2019-12-02 19:08:32 -0800

    docs: make a few small tweaks to docs

--- scrape.ts
+++ scrape.ts
@@ -3,7 +3,7 @@ import scrapeIt, { ScrapeOptions } from 'scrape-it'
 /**
  * Scrapes a URL using the [scrape-it](https://github.com/IonicaBizau/scrape-it#documentation) API
  *
- * @param url - Link to URL to scrape.
+ * @param url - URL to scrape
  * @param opts - Scraping options as described in the [scrape-it readme](https://github.com/IonicaBizau/scrape-it#documentation)
  */
 export default async (url: string, opts: object): Promise<object> => {
